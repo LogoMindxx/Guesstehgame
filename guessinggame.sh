f06056c0d58e0ecb19c127db7c4cd1bbf6f362df
LogoMindxx <66810682+LogoMindxx@users.noreply.github.com>
2020-06-15 12:13:41 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
-function guess(){
+function guess()
+{
     true_ans=$(ls -l |grep "^-"|wc -l)
     while true;
-    do
-    echo "Enter your Number:"
-    read  number
-    if [ $number -lt $true_ans 
-    then
-    echo "Your number is less then the actual number:"
-    elif [ $number -gt $true_ans ]
-    then
-    echo "Your number is greater then the actual number:"
-    else
-    echo "Well done!"
-    break;
-    fi
+     do
+       echo "Enter your Number:"
+       read  number
+       
+       if [ $number -lt $true_ans 
+       then
+           echo "Your number is less then the actual number:"
+       elif [ $number -gt $true_ans ]
+       then
+           echo "Your number is greater then the actual number:"
+       else
+           echo "Well done!"
+       break;
+   
+  fi
     done
 }
  
